a22d033fa4f5fab5602e1b6ff60bf42d4f3df1ff
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-14 22:33:48 +0200

    Update fmt

--- main.tf
+++ main.tf
@@ -5,17 +5,17 @@ locals {
 
 ### If we have SecureString, create the datasource for the kms_key
 data "aws_kms_key" "this" {
-  count = signum(length(lookup(local.parameters_distinct_on_type,"SecureString",[])))
-	key_id = var.kms_alias
+  count  = signum(length(lookup(local.parameters_distinct_on_type, "SecureString", [])))
+  key_id = var.kms_alias
 }
 
 resource "aws_ssm_parameter" "this" {
-	for_each = { for parameter in var.parameters : parameter.name => parameter }
+  for_each = { for parameter in var.parameters : parameter.name => parameter }
 
-	name      = format("%s%s", var.name_prefix, each.value.name)
-	type      = lookup(each.value, "type", "SecureString")
-	value     = each.value.value
-	key_id    = lookup(each.value, "type", "SecureString") != "SecureString" ? null : concat(data.aws_kms_key.this.*.arn, [""])[0]
-	overwrite = var.overwrite
-	tags      = var.tags
+  name      = format("%s%s", var.name_prefix, each.value.name)
+  type      = lookup(each.value, "type", "SecureString")
+  value     = each.value.value
+  key_id    = lookup(each.value, "type", "SecureString") != "SecureString" ? null : concat(data.aws_kms_key.this.*.arn, [""])[0]
+  overwrite = var.overwrite
+  tags      = var.tags
 }
